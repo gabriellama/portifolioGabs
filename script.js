03e2e4a6854c6b1b2a0c00a06bfe187f1e7ab0ad
gabriellama <gabriellamalvares@gmail.com>
2023-08-07 01:09:26 -0300

    att commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const btn = document.getElementById('btn')
+const btn = document.getElementById("btn");
+const darkmode = localStorage.getItem("dark");
+if (darkmode !== 'true') {
+    btn.checked = false;
+    document.body.classList.remove("dark");
+} else {
+    btn.checked = true;
+    document.body.classList.add("dark");
+}
 
-btn.addEventListener('change', (e) => {
-    localStorage.setItem("dark", e.target.checked);
-    document.body.classList.toggle("dark", e.target.checked);
-})
-
-// const darkmode = localStorage.getItem("dark");
-// btn.checked = darkmode
-    // document.body.classList.toggle("dark", darkmode);
+btn.addEventListener("change", (e) => {
+  document.body.classList.toggle("dark", e.target.checked);
+  localStorage.setItem("dark", e.target.checked);
+});
